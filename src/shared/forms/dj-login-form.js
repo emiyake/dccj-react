b10f029b0412f5f9ea663fc11c957024a85a1af2
Davi Correia <davicorreiajr@gmail.com>
2017-09-12 21:57:51 -0300

    Added handleInputChange method

--- src/shared/forms/dj-login-form.js
+++ src/shared/forms/dj-login-form.js
@@ -5,20 +5,57 @@ import DjButton from '../buttons/dj-button.js';
 import DjInput from '../inputs/dj-input.js';
 
 class DjLoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password: '' };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.username);
+    alert('A pw was submitted: ' + this.state.password);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="form">
+      <form className="form" onSubmit={this.handleSubmit}>
         <h2>Faça login!</h2>
         <div className="formInput">
-          <DjInput type="text" name="username" placeholder="username"/>
+          <input 
+            className = "input"
+            type = "text"
+            name = "username"
+            placeholder = "username"
+            onChange = { this.handleInputChange } 
+          />
         </div>
         <div className="formInput">
-          <DjInput type="password" name="password" placeholder="password" />
+          <input 
+            className = "input"
+            type = "password"
+            name = "password"
+            placeholder = "password"
+            onChange = { this.handleInputChange } 
+          />
         </div>
         <div>
           <DjButton label="Login" />
         </div>
-      </div>
+
+        <div>
+          <p>username: { this.state.username }</p>
+          <p>password: { this.state.password }</p>
+        </div>
+      </form>
     )
   }
 }
