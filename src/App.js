1ded8a50434f2b8fc2606e34f074288ed5135f5c
Davi Correia <davicorreiajr@gmail.com>
2017-09-16 14:13:27 -0300

    Changed Clock of folder and its name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-import Clock from './tests/Clock.js'
+import Clock from './modules/clock/dj-clock.js'
 import DjLoginForm from './modules/forms/dj-login-form.js'
 
 class App extends Component {
